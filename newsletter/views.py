a111b01407fe2ff73eed435ba84320072c517233
Sagar Hani <sagarhani33@gmail.com>
2015-08-24 22:42:20 +0530

    Added a form on home page, email ID should ends with .edu

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
+from .forms import SignUpForm, ContactForm
 # Create your views here.
+def home(request):
+	title = "Welcome!"
+	form = SignUpForm(request.POST or None)
+
+	context = {
+	    "title": title,
+	    "form": form,
+	}
+
+
+	if form.is_valid():
+		form.save()
+		instance = form.save(commit=False)
+
+		full_name = form.cleaned_data.get("full_name")
+
+		#if not instance.full_name:
+		#	instance.full_name == "Baymax"
+		instance.save()
+		context = {
+		"title" : "Thank You"
+		}
+		
+	return render(request, "home.html", context)
+    
+
+def contact(request):
+	form = ContactForm(request.POST or None)
+	context = {
+		"form" : form,
+	}
+	return render(request, "forms.html", context)
--- trydjango18/urls.py
+++ trydjango18/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'trydjango18.views.home', name='home'),
+     url(r'^$', 'newsletter.views.home', name='home'),
+     url(r'^contact/$', 'newsletter.views.contact', name='contact'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
